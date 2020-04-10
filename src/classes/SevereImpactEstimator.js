9eb0528e4b26223ff7a7432f2dbb3bb6b7e3a38b
odinaka-joy <odinakajoy909@gmail.com>
2020-04-10 02:22:31 +0100

    completes challenge one [fixes error - infectionsByRequestedTime retu…

--- src/classes/SevereImpactEstimator.js
+++ src/classes/SevereImpactEstimator.js
@@ -1,6 +1,30 @@
 class SevereImpactEstimator {
-  constructor(reportedCases) {
+  constructor(periodType, timeToElapse, reportedCases) {
+    this.periodType = periodType;
+    this.timeToElapse = timeToElapse;
     this.reportedCases = reportedCases;
+  }
+
+  requestedTime() {
+    let days;
+    if (this.periodType === 'days') {
+      if (this.timeToElapse > 2) {
+        days = Math.floor(this.timeToElapse / 3);
+      } else {
+        days = 1;
+      }
+    }
+    if (this.periodType === 'weeks') {
+      // Converts timeToElapse in weeks to days
+      const toDays = this.timeToElapse * 7;
+      days = Math.floor(toDays / 3);
+    }
+    if (this.periodType === 'months') {
+      // Converts timeToElapse in months to days
+      const toDays = this.timeToElapse * 30;
+      days = Math.floor(toDays / 3);
+    }
+    return days;
   }
 
   currentlyInfected() {
@@ -8,8 +32,8 @@ class SevereImpactEstimator {
   }
 
   infectionsByRequestedTime() {
-    // infenction rate is estimated based on the fact that currentlyInfected doubles every 3 days
-    return (this.reportedCases * 50) * (2 ** 9);
+    const lengthInDays = this.requestedTime();
+    return (this.reportedCases * 10) * (2 ** lengthInDays);
   }
 }
 export default SevereImpactEstimator;
