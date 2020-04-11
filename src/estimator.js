ee9febca5a3c88dbdf32546bd573d53bfef3de60
odinaka-joy <odinakajoy909@gmail.com>
2020-04-11 16:39:44 +0100

    fixes issues in challenge two updated

--- src/estimator.js
+++ src/estimator.js
@@ -6,19 +6,17 @@ const covid19ImpactEstimator = (data) => {
   const impact = new ImpactEstimator(data);
   const severeImpact = new SevereImpactEstimator(data);
   return {
-    estimate: {
-      impact: {
-        currentlyInfected: impact.currentlyInfected(),
-        infectionsByRequestedTime: impact.infectionsByRequestedTime(),
-        severeCasesByRequestedTime: impact.severeCasesByRequestedTime(),
-        hospitalBedsByRequestedTime: impact.hospitalBedsByRequestedTime()
-      },
-      severeImpact: {
-        currentlyInfected: severeImpact.currentlyInfected(),
-        infectionsByRequestedTime: severeImpact.infectionsByRequestedTime(),
-        severeCasesByRequestedTime: severeImpact.severeCasesByRequestedTime(),
-        hospitalBedsByRequestedTime: severeImpact.hospitalBedsByRequestedTime()
-      }
+    impact: {
+      currentlyInfected: impact.currentlyInfected(),
+      infectionsByRequestedTime: impact.infectionsByRequestedTime(),
+      severeCasesByRequestedTime: impact.severeCasesByRequestedTime(),
+      hospitalBedsByRequestedTime: impact.hospitalBedsByRequestedTime()
+    },
+    severeImpact: {
+      currentlyInfected: severeImpact.currentlyInfected(),
+      infectionsByRequestedTime: severeImpact.infectionsByRequestedTime(),
+      severeCasesByRequestedTime: severeImpact.severeCasesByRequestedTime(),
+      hospitalBedsByRequestedTime: severeImpact.hospitalBedsByRequestedTime()
     }
   };
 };
