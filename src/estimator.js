ec2af5dd588fe21091d05141d61ab525c8f71847
odinaka-joy <odinakajoy909@gmail.com>
2020-04-11 04:29:23 +0100

    completes challenge one [fixes error in covid19ImpactEstimator() output]

--- src/estimator.js
+++ src/estimator.js
@@ -4,20 +4,24 @@ import SevereImpactEstimator from './classes/SevereImpactEstimator';
 
 const covid19ImpactEstimator = (data) => {
   const { periodType, timeToElapse, reportedCases } = data;
-  const impactCalc = new ImpactEstimator(periodType, timeToElapse, reportedCases);
-  const severeImpactCalc = new SevereImpactEstimator(periodType, timeToElapse, reportedCases);
-  const impact = {
-    currentlyInfected: impactCalc.currentlyInfected(),
-    infectionsByRequestedTime: impactCalc.infectionsByRequestedTime()
-  };
-  const severeImpact = {
-    currentlyInfected: severeImpactCalc.currentlyInfected(),
-    infectionsByRequestedTime: severeImpactCalc.infectionsByRequestedTime()
-  };
+  const impact = new ImpactEstimator(periodType, timeToElapse, reportedCases);
+  const severeImpact = new SevereImpactEstimator(periodType, timeToElapse, reportedCases);
+  // const impact = {
+  //   currentlyInfected: impactCalc.currentlyInfected(),
+  //   infectionsByRequestedTime: impactCalc.infectionsByRequestedTime()
+  // };
+  // const severeImpact = {
+  //   currentlyInfected: severeImpactCalc.currentlyInfected(),
+  //   infectionsByRequestedTime: severeImpactCalc.infectionsByRequestedTime()
+  // };
   return {
-    estimate: {
-      impact,
-      severeImpact
+    impact: {
+      currentlyInfected: impact.currentlyInfected(),
+      infectionsByRequestedTime: impact.infectionsByRequestedTime()
+    },
+    severeImpact: {
+      currentlyInfected: severeImpact.currentlyInfected(),
+      infectionsByRequestedTime: severeImpact.infectionsByRequestedTime()
     }
   };
 };
