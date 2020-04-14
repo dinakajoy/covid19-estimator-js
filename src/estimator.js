e3c40f7d9c360f7196ac0e5125391f9f0f699adf
odinaka-joy <odinakajoy909@gmail.com>
2020-04-14 06:18:53 +0100

    updates estimator.js file

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,6 @@
-import ImpactEstimator from './classes/ImpactEstimator';
-import SevereImpactEstimator from './classes/SevereImpactEstimator';
+/* eslint-disable import/extensions */
+import ImpactEstimator from './classes/ImpactEstimator.js';
+import SevereImpactEstimator from './classes/SevereImpactEstimator.js';
 
 const covid19ImpactEstimator = (data) => {
   const impact = new ImpactEstimator(data);
@@ -27,39 +28,5 @@ const covid19ImpactEstimator = (data) => {
   };
   return output;
 };
-
-// Inputed data
-
-// const processInput = () => {
-//   const name = document.querySelector('#country').value;
-//   const avgAge = document.querySelector('#averageAge').value;
-//   const avgDailyIncomeInUSD = document.querySelector('#averageDailyIncomeInUSD').value;
-//   const avgDailyIncomePopulation = document.querySelector('#averageDailyIncomePopulation').value;
-//   const periodType = document.querySelector('#periodType').value;
-//   const timeToElapse = document.querySelector('#timeToElapse').value;
-//   const reportedCases = document.querySelector('#reportedCases').value;
-//   const population = document.querySelector('#population').value;
-//   const totalHospitalBeds = document.querySelector('#totalHopitalBeds').value;
-//   const data = {
-//     region: {
-//       name,
-//       avgAge,
-//       avgDailyIncomeInUSD,
-//       avgDailyIncomePopulation
-//     },
-//     periodType,
-//     timeToElapse,
-//     reportedCases,
-//     population,
-//     totalHospitalBeds
-//   };
-//   covid19ImpactEstimator(data);
-// };
-
-// const submit = document.querySelector('#data-go-estimate');
-// submit.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   processInput();
-// });
 
 export default covid19ImpactEstimator;
